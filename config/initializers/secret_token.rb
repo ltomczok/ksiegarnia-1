902dc2f26b27ff221db48fcc1e19e07b41df37e2
prest <t.gubala@maxsote.pl>
2012-11-20 11:49:02 +0100

    nowy railsik

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksiegarnia::Application.config.secret_token = 'f43739ee1b4c91c553c97b7702665568d00adab1e687e7efe59f21ddd2209b786c22a4228865e16696b8e58ea1f329692e88ecd8a6758449ca6778d4dcee7b45'
